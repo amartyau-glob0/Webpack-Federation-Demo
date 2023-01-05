7a161d0c7d49b014507b0638d53545a8e119514e
Amartya Utkarsh <amartya.utkarsh@globality.com>
2023-01-06 01:24:28 +0530

    add PB code

---
+++ project-brief/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Project Brief has been successfully loaded in ..... </h3>
+    </div>
+  );
+}
+
+export default App;
